2176340eb9fab063a6a3b19fb78f683048087a5e
azizseddik <esseddikmedaziz@gmail.com>
2025-02-26 11:04:26 +0100

    Update SecurityConfig.java

--- src/main/java/Security/SecurityConfig.java
+++ src/main/java/Security/SecurityConfig.java
@@ -4,7 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class SecurityConfig {
+public class SecurityConfig { 
+@Autowired
+    UserDetailsService userDetailsService ;
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/save").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
